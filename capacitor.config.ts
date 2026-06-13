c374a3b26e804cecdb67fac0aa6633b781de3b25
Donaldson Tega <sogolodonald11@gmail.com>
2026-06-13 19:07:37 +0100

    hrtr

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.vella.ai',
-  appName: 'vella',
-  webDir: 'dist'
-};
-
-export default config;
